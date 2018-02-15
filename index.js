5f3af8ac3e237a18b89f540d30e5ac97fa6cd426
twishasaraiya <saraiyatwisha@gmail.com>
2018-02-15 23:32:42 +0530

    added actions

--- index.js
+++ index.js
@@ -16,60 +16,101 @@ app.listen(process.env.PORT || 1337 , () => {
 app.post('/webhook',(req,res) => {
   console.log(req.body);
   //CALL NEWS API AND EXTRACT PARAMETERS FROM REQ.BODY.PARAMETERS
-  console.log(req.body.result.parameters.Topics);
-  var topicToSearch = req.body.result.parameters.Topics ? req.body.result.parameters.Topics : 'technology' ;
+  const ACTION=req.body.result.action;
+  console.log('action',ACTION);
 
-  console.log(topicToSearch);
   var country = req.body.result.parameters.geo-country ? req.body.result.parameters.geo-country : 'in';
 
-  var url = encodeURI("http://newsapi.org/v2/top-headlines?country=in&apiKey=" + apiKey);
 
-  console.log(url);
-  http.get(url, (responseFromApi) => {
-    //console.log(responseFromApi);
-    let body="";
-    responseFromApi.on('data',(newsData) => {
-      body+=newsData;
-    });
+  if(ACTION==="SPECIFIC_NEWS"){
+    console.log(req.body.result.parameters.about);
+    var about = req.body.result.parameters.about ? req.body.result.parameters.about : null  ;
+      console.log('About',about);
 
-    responseFromApi.on("end",()=>{
-      body=JSON.parse(body);
-      //console.log(body);
-      var num=body.totalResults;
-      console.log(num);
-      console.log('num=',num);
-      var response = " Okay I found " + num.toString() + " news stories for you!";
-      var items=[];
-      body.articles.forEach((news)=>{
-          var obj={};
-          obj['description']=news.description;
-          obj['image']={
-            "url":news.urlToImage
-          };
-          obj['url']=news.url;
-          obj['title']=news.title;
-        //  console.log(obj);
-          items.push(obj);
+      var url = encodeURI("http://newsapi.org/v2/top-headlines?country=in&category="+about+"&apiKey="+apiKey);
+      console.log('URL=',url);
+
+      http.get(url, (responseFromApi)=>{
+        let body = "";
+        responseFromApi.on('data',(newsData)=>{
+          body+=newsData;
+        });
+
+        responseFromApi.on("end",()=>{
+          body=JSON.parse(body);
+          var num = body.totalResults;
+          console.log(num);
+
+          var items = [];
+          body.articles.forEach((news)=>{
+            var obj={};
+            obj['description']=news.description;
+            obj['image']={
+              "url":news.urlToImage
+            };
+            obj['url']=news.url;
+            obj['title']=news.title;
+            items.push(obj);
+          });
+          res.json({
+            "displayText":"Okay here is some news for you!!",
+            "messages":[
+              {
+                "platform":"google",
+                "items":items,
+                "type":"carousel_card"
+              }
+            ]
+          });
+
+
+        });
       });
-      //console.log(items);
-      res.json({
-        "messages":[
-          {
-            "platform":"google",
-            "items": items,
-            "type":"carousel_card"
-          }
-        ]
+  }
+  else if(ACTION === 'BREAKING_NEWS'){
+    var date=req.body.result.parameters.date;
+    var url = encodeURI("http://newsapi.org/v2/top-headlines?country=in&from="+date+"&to="+date+"&apiKey=" + apiKey);
+
+    console.log(url);
+    http.get(url, (responseFromApi) => {
+      //console.log(responseFromApi);
+      let body="";
+      responseFromApi.on('data',(newsData) => {
+        body+=newsData;
       });
+
+      responseFromApi.on("end",()=>{
+        body=JSON.parse(body);
+        //console.log(body);
+        var num=body.totalResults;
+        console.log(num);
+        console.log('num=',num);
+        var response = " Okay I found " + num.toString() + " news stories for you!";
+        var items=[];
+        body.articles.forEach((news)=>{
+            var obj={};
+            obj['description']=news.description;
+            obj['image']={
+              "url":news.urlToImage
+            };
+            obj['url']=news.url;
+            obj['title']=news.title;
+          //  console.log(obj);
+            items.push(obj);
+        });
+        //console.log(items);
+        res.json({
+          "messages":[
+            {
+              "platform":"google",
+              "items": items,
+              "type":"carousel_card"
+            }
+          ]
+        });
+      });
+
     });
+  }
 
-  },(error) =>{
-
-    var response = "this is a sample webhook response";
-    res.status(200).json({
-      speech: response,
-      displayText: response,
-    });
-
-  });
 });
